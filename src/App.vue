285cd188ac16a17bb4d9fe81f31d7991375275a6
hfren <hfren@Ctrip.com>
2017-05-08 18:20:32 +0800

    新增静态页

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,5 @@ export default {
 }
 </script>
 <style lang="scss">
-  @import './style/style'
+  @import './style/style';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'bootstrap'
+// import bootstrap from 'bootstrap'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import api from './config/api'
 
-Vue.use(VueRouter)
-
 import App from './App.vue'
 import routes from './config/routes'
 
+// 放在sass引入css文件会报错，也不知道为什么
+import bootstrap from './style/3rd/bootstrap.css'
+
+Vue.use(VueRouter);
 Vue.prototype.$api = api;
 
 const router = new VueRouter({
